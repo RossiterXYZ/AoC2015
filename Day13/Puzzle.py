37ff9f77f7abfa9163f48109bc8edfa54f0dadf3
Michael Rossiter <michael@rossiter.xyz>
2022-12-05 23:28:25 +0800

    Day13

--- Day13/Puzzle.py
+++ Day13/Puzzle.py
@@ -1,6 +1,7 @@
+Puzzle2 = False # Toggle for answer 1 or 2
 PersonSet = set()
 HappinessMap = {}
-with open("Day13\InputTest.txt") as InputFile:
+with open("Day13\Input.txt") as InputFile:
     for InputLine in InputFile:
         Person1, _, Positive, Value, _, _, _, _, _, _, Person2 = InputLine[:len(InputLine) - 2].split(' ')
         Value = int(Value)
@@ -10,7 +11,38 @@ with open("Day13\InputTest.txt") as InputFile:
             HappinessMap[Person1] = {}
             PersonSet.add(Person1)
         HappinessMap[Person1].update({Person2: Value})
+if Puzzle2:
+    print('Please be patient, this takes some time!')
+    PersonSet.add('Me')
+    HappinessMap['Me'] = {}
+    for Person in PersonSet:
+        HappinessMap['Me'].update({Person: 0})
+        HappinessMap[Person].update({'Me': 0})
 
-print(HappinessMap)
-print(f'Puzzle 1: 1 = {1}')
-print(f'Puzzle 2: 1 = {1}')
+SeatingArrangements = [[]]
+for _ in PersonSet:
+    SeatingCopy = SeatingArrangements.copy()
+    SeatingArrangements = []
+    for SeatingList in SeatingCopy:
+        for Person in PersonSet:
+            if Person in SeatingList:
+                continue
+            TmpList = SeatingList.copy()
+            TmpList.append(Person)
+            SeatingArrangements.append(TmpList)
+for Item in range(len(SeatingArrangements)):
+    SeatingArrangements[Item].append(SeatingArrangements[Item][0])
+
+Largest = 0
+for Arrangement in SeatingArrangements:
+    Sum = 0
+    for Index in range(len(Arrangement) - 1):
+        Sum += HappinessMap[Arrangement[Index]][Arrangement[Index+1]]
+        Sum += HappinessMap[Arrangement[Index+1]][Arrangement[Index]]
+    if Sum > Largest:
+        Largest = Sum
+
+if not Puzzle2:
+    print(f'Puzzle 1: Everyone Else = {Largest}')
+else:
+    print(f'Puzzle 2: Including Me = {Largest}')
